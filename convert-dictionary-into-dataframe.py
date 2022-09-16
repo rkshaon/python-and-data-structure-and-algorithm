0b261eb467baf0b541cf59066b86c7eb47d3b682
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2022-09-16 17:17:05 +0600

    converting dictonary into dataframe - output 2

--- convert-dictionary-into-dataframe.py
+++ convert-dictionary-into-dataframe.py
@@ -8,12 +8,24 @@ emp_details = {
     }
 }
 
-# Output
+# Output 1
 #                Jessica                 Alba          Scarlett  Johansson
 # ID                 001                  002               003        004
 # Salary            2000                 1800              1500       1200
 # Designation  Team Lead  Associate Team Lead  Senior Developer  Developer
 
+# Output 2
+#             ID Salary          Designation
+# Jessica    001   2000            Team Lead
+# Alba       002   1800  Associate Team Lead
+# Scarlett   003   1500     Senior Developer
+# Johansson  004   1200            Developer
+
 import pandas as pd
 
 data_frame = pd.DataFrame(emp_details["Employee"])
+
+# Output 1
+print(data_frame)
+# Output 2
+print(data_frame.T)

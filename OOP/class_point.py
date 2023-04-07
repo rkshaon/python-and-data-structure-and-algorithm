f5a72a85e541377919f5625a456f3503d910b0b0
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-04-08 01:39:54 +0600

    OOP - operator over loading

--- OOP/class_point.py
+++ OOP/class_point.py
@@ -1,11 +1,33 @@
 import matplotlib.pyplot as plt
 
-
-
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    # Operator overloading
+    def __add__(self, other):
+        if isinstance(other, Point):
+            x = self.x + other.x
+            y = self.y + other.y
+        else:
+            x = self.x + other
+            y = self.y + other
+
+        return Point(x, y)
+        
     def plot(self):
-        pass
+        plt.scatter(self.x, self.y)
+
+a = Point(1, 1)
+b = Point(2, 2)
+c = a + b
+# d = Point(5, 5)
+# c += d
+d = a + 5
+
+# print(c.x, c.y)
+# c.plot()
+d.plot()
+# a.plot()
+plt.show()

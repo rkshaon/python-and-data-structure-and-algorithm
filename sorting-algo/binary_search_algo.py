4ad7af27b81f6cd5af4c8fc4c53245f3a006d989
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2023-04-08 17:48:06 +0600

    Binary Search Algorithm

--- sorting-algo/binary_search_algo.py
+++ sorting-algo/binary_search_algo.py
@@ -57,6 +57,21 @@ def binarySearchIterative(v, To_Find):
     else:
         print("Not Found")
 
+# def binary_search(arr, target):
+#     left = 0
+#     right = len(arr) - 1
+    
+#     while left <= right:
+#         mid = (left + right) // 2
+#         if arr[mid] == target:
+#             return mid
+#         elif arr[mid] < target:
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+            
+#     return -1
+
 
 # Recursive method
 def binarySearchRecursive(arr, l, r, x):
